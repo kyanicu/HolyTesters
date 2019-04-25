cbc4ea510940d2274e82c3a584845bcd7c059957
yunatatski <yunatatski@hotmail.com>
2019-04-25 10:14:42 -0400

    Navmesh and character model

---
+++ Assets/Scripts/EnemyNavAgent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavAgent : MonoBehaviour
+{
+    GameObject player;
+    private Transform playerTransform;
+    NavMeshAgent agent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+        //playerTransform = playerTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //agent.destination = player.transform;
+    }
+}
