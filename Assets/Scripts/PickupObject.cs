3406142c3d2e44f7683ee2a005a6f018a8ae2e0b
yunatatski <yunatatski@hotmail.com>
2019-05-01 16:26:54 -0400

    Added HP regen and some inventory code

---
+++ Assets/Scripts/PickupObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObject : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject itemButton;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    //Destroy(gameObject);
+                    break;
+                }
+            }
+            Debug.Log("picked up");
+            //Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/Player_Status.cs
+++ Assets/Scripts/Player_Status.cs
@@ -10,31 +10,67 @@ using UnityEngine.UI;
 public class Player_Status : MonoBehaviour
 {
     [SerializeField] private float health;
+    [SerializeField] private float maxHealth;
+    private bool isRegenHealth;
     [SerializeField] private Slider healthbar;
+    [SerializeField] private float healthRegenSpeed;
+    [SerializeField] private float healthRegenAmount;
+
     // Start is called before the first frame update
     void Start()
     {
         health = 100;
+        maxHealth = 100;
+        healthRegenSpeed = 2;
+        healthRegenAmount = 3;
+        isRegenHealth = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (health != maxHealth && !isRegenHealth)
+        {
+            StartCoroutine(regainHealthOverTime());
+        }
+
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         healthbar.value = health;
     }
 
-    public void setHealth(float amount) {
+    public void setHealth(float amount)
+    {
         health = amount;
     }
-    public float getHealth() {
+    public float getHealth()
+    {
         return health;
     }
 
-    public void takeDamage(float amount) {
+    public void takeDamage(float amount)
+    {
         health -= amount;
+    }
+
+    public void regenHealth()
+    {
+        if (health < maxHealth) {
+            health += healthRegenAmount;
+        }
+    }
+
+    private IEnumerator regainHealthOverTime()
+    {
+        isRegenHealth = true;
+        while (health < maxHealth)
+        {
+            regenHealth();
+            yield return new WaitForSeconds(healthRegenSpeed);
+        }
+        isRegenHealth = false;
     }
 }
